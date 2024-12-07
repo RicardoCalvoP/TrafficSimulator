b707e2248d87b5a3be365853fdade4289535c6c8
RicardoCalvoP <a01028889@tec.mx>
2024-12-07 00:00:10 -0600

    bar chart is shown, change to piechart or change from agent to model

--- Server/2D_Server.py
+++ Server/2D_Server.py
@@ -3,56 +3,66 @@ Ricardo Calvo Pérez
 
 Starting Date: 12/2024
 """
-from agents import Car, Traffic_Light, Road, Obstacle, Destination
-from model import City
+from model import City, Car, Traffic_Light, Road, Obstacle, Destination
 from mesa.visualization import CanvasGrid, ModularServer, BarChartModule
-import random
+
+DIRECTIONS = {"Right": "→",
+              "Left": "←",
+              "Up": "↑",
+              "Down": "↓"}
 
 
 def agent_portrayal(agent):
     if agent is None:
         return
 
-    portrayal = {"Shape": "rect",
-                 "Filled": "true",
-                 "Layer": 1,
-                 "w": 1,
-                 "h": 1
+    portrayal = {
+        "Shape": "rect",
+        "Filled": True,
+        "Layer": 0,
+        "w": 1,
+        "h": 1,
+        "text": "",
+        "text_color": "black"
+    }
 
-                 }
-
-    if (isinstance(agent, Car)):
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-
+    # Cars
+    if isinstance(agent, Car):
         portrayal["Shape"] = "circle"
         portrayal["Filled"] = "true"
         portrayal["r"] = 0.69
-        portrayal["Layer"] = 2
         portrayal["Color"] = agent.color
-    if (isinstance(agent, Road)):
-        portrayal["Color"] = "grey"
-        portrayal["Layer"] = 0
-
-    if (isinstance(agent, Traffic_Light)):
-        portrayal["Color"] = "red" if not agent.condition else "green"
         portrayal["Layer"] = 1
-        portrayal["w"] = 0.8
-        portrayal["h"] = 0.8
 
-    if (isinstance(agent, Obstacle)):
-        portrayal["Color"] = "cadetblue"
+    # Traffic Lights
+    if isinstance(agent, Traffic_Light):
+        portrayal["Shape"] = "circle"
+        portrayal["Filled"] = "true"
+        portrayal["r"] = 0.5
+        portrayal["Color"] = "#B81B0E" if not agent.condition else "#549C30"
+        portrayal["Layer"] = 1
+
+    # Roads
+    if isinstance(agent, Road):
+        portrayal["text"] = DIRECTIONS[agent.direction]
+        portrayal["text_color"] = "white"
+        portrayal["stroke"] = None
+        portrayal["Color"] = "#3F3F3F"
         portrayal["Layer"] = 0
-        portrayal["w"] = 0.8
-        portrayal["h"] = 0.8
-    if (isinstance(agent, Destination)):
-        portrayal["Color"] = "pink"
+
+    # Obstacles
+    if isinstance(agent, Obstacle):
+        portrayal["stroke"] = None
+        portrayal["Color"] = "#306E68"
         portrayal["Layer"] = 0
-        portrayal["w"] = 0.8
-        portrayal["h"] = 0.8
+
+    # Destinations
+    if isinstance(agent, Destination):
         portrayal["text"] = agent.carsArrived
         portrayal["text_color"] = "black"
+        portrayal["stroke"] = None
+        portrayal["Color"] = "#5DC1B9"
+        portrayal["Layer"] = 0
 
     return portrayal
 
@@ -63,21 +73,21 @@ with open(cityFile) as baseFile:
     width = len(lines[0])  # Correct width without '\n'
     height = len(lines)  # Number of lines gives the height
 
-
 model_params = {
     "width": width,
     "height": height,
     "city": cityFile
 }
 
-cars_arrived_chart = BarChartModule(
-    {"Label": "Arrived", "Color": "#00FF00"}
-)
-
 grid = CanvasGrid(agent_portrayal, width, height, width*20, height*20)
 
+
+cars_arrived_chart = BarChartModule([{"Label": "Arrived", "Color": "#00FF00"}],
+                                    scope="agent", sorting="ascending", sort_by="Arrived")
+
+
 server = ModularServer(
-    City, [grid], "Traffic Base", model_params)
+    City, [grid, cars_arrived_chart], "Traffic Base", model_params)
 
 server.port = 8889  # The default
 server.launch()
